a42977325b06d3d4905a71f232c107c42864acb8
Gabriel <gbarros0495@gmail.com>
2023-09-26 22:50:16 -0300

    Aprendendo sobre funções em Javascript

---
+++ Estudos/Modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes";
+
+console.log(mostraIdade('Gabriel', 27));
+console.log(mostraCidade('Gabriel', 'Praia Grande'));
+console.log(mostraHobby('Gabriel', 'reformar minha casa'));
